0bb91f85ca07e654ca913700a41aa6b48ac292ac
DingoFreestyle <104494672+DingoFreestyle@users.noreply.github.com>
2022-04-29 20:39:15 +0900

    복습 겸 버전업 진행

--- day02_1.py
+++ day02_1.py
@@ -31,34 +31,88 @@
 
 ############################### 튜터님의 대답
 
+# import random
+#
+# option = ['가위', '바위', '보']
+# computer = random.randint(0, 2)
+# computer_value = option[computer]
+#
+# while True :
+#     player_value = input('가위 바위 보?')
+#     if player_value in option:
+#         break
+#     print('제대로 입력바람!!!')
+#
+#
+# print(f'플레이어는 {player_value}를 선택, 컴퓨터는 {computer_value}를 선택했습니다')
+# if computer_value == player_value:
+#     print('비겼습니다')
+# elif player_value == '가위':
+#     if computer_value == '바위':
+#         print('졌습니다')
+#     else:
+#         print('이겼습니다')
+# elif player_value == '바위':
+#     if computer_value == '보':
+#         print('졌습니다')
+#     else:
+#         print('이겼습니다')
+# elif player_value == '보':
+#     if computer_value == '가위':
+#         print('졌습니다')
+#     else:
+#         print('이겼습니다')
+
+
+ # 가위바위보 버전업!
+
 import random
 
-option = ['가위', '바위', '보']
-computer = random.randint(0, 2)
-computer_value = option[computer]
+print('가위바위보를 시작합니다')
+score = 1
+count = 0
 
-while True :
-    player_value = input('가위 바위 보?')
-    if player_value in option:
+while True:
+    option = ['가위', '바위', '보']
+    computer = random.randint(0, 2)
+    computer_value = option[computer]
+    count += 1
+    while True:
+        player_value = input('가위 바위 보?')
+        if player_value in option:
+            break
+        print('제대로 입력바람!!!')
+
+    print(f'플레이어는 {player_value}를 선택, 컴퓨터는 {computer_value}를 선택했습니다')
+    if computer_value == player_value:
+        print('비겼습니다')
+
+    elif player_value == '가위':
+        if computer_value == '바위':
+            print('졌습니다')
+            score -= 1
+        else:
+            score += 1
+            print('이겼습니다')
+    elif player_value == '바위':
+        if computer_value == '보':
+            print('졌습니다')
+            score -= 1
+        else:
+            score += 1
+            print('이겼습니다')
+    elif player_value == '보':
+        if computer_value == '가위':
+            print('졌습니다')
+            score -= 1
+        else:
+            score += 1
+            print('이겼습니다')
+    print(f'{count} 시도 중 현재 스코어는 {score}점')
+
+    if score > 10:
+        print('승리했습니다! 가위바위보의 신!')
         break
-    print('제대로 입력바람!!!')
-
-
-print(f'플레이어는 {player_value}를 선택, 컴퓨터는 {computer_value}를 선택했습니다')
-if computer_value == player_value:
-    print('비겼습니다')
-elif player_value == '가위':
-    if computer_value == '바위':
-        print('졌습니다')
-    else:
-        print('이겼습니다')
-elif player_value == '바위':
-    if computer_value == '보':
-        print('졌습니다')
-    else:
-        print('이겼습니다')
-elif player_value == '보':
-    if computer_value == '가위':
-        print('졌습니다')
-    else:
-        print('이겼습니다')
+    elif score == 0:
+        print('패배했습니다! 루저!')
+        break
