82adc5defcfa7badefe73d40eb450ae7e35bd24a
DingoFreestyle <104494672+DingoFreestyle@users.noreply.github.com>
2022-04-29 21:50:49 +0900

    파이썬 기초 def 함수 배운 것 필기

---
+++ day03.py
@@ -0,0 +1,34 @@
+########## Define에 대한 수업#################
+
+# def my_function(name):
+#     print("hello, my friend " + name);
+# my_function("james")
+# my_function("ohn")
+# my_function("mina")
+
+##############################################
+###### def는 return을 해줘야 아웃풋을 보여준다#####
+
+# def my_function(xyz):
+#     words = xyz + " hello, my dear."
+# print(my_function("hobin"))
+# # > 결과는 None 이 나온다. def을 통한 연산은 return을 넣어줘야 한다
+
+def my_function(xyz):
+    words = xyz + " hello, my dear."
+    return words
+print(my_function("hobin"))
+# # 결과는 hobin hello, my dear. 라고 나온다.
+
+
+
+
+
+
+
+
+
+
+
+
+
